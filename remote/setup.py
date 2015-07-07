7cfe27d5759223282c458d3b74e65ffb3e1ec851
hackolite <laureote-loic@hotmail.fr>
2015-07-07 18:35:24 +0200

    Create setup.py

---
+++ remote/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name = "remote",
+    version = "0.1",
+    packages = find_packages(),
+    author = "loic.laureote",
+    author_email = "loic.laureote@hotmail.com",
+)
